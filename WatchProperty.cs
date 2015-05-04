a7fcf1ae8fb188fc387e4aec1d6eec41c6e2bd2b
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-04 16:45:56 +0200

    Initial commit

---
+++ WatchProperty.cs
@@ -0,0 +1,236 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Reflection;
+
+namespace WatchProperty {
+
+    /// Observe component property and act when value is changed.
+    public class WatchProperty : MonoBehaviour {
+
+        /// Triggers on the source component that trigger actions on
+        /// the target component.
+        public enum Triggers {
+
+            Equal,
+            EqualOrLess,
+            LessThan,
+            MoreThan
+
+        }
+
+        /// Actions that can be done to the target component.
+        public enum Actions {
+
+            Enable,
+            Disable,
+            Set
+
+        }
+
+        /// Source component.
+        ///
+        /// Component which property will be used to update
+        /// target game object.
+        [SerializeField]
+        private Component _sourceCo;
+
+        public Component SourceCo {
+            get { return _sourceCo; }
+            set { _sourceCo = value; }
+        }
+
+        /// Target component.
+        ///
+        /// Component which property will be updated by
+        /// target game object's property.
+        [SerializeField]
+        private Component _targetCo;
+
+        public Component TargetCo {
+            get { return _targetCo; }
+            set { _targetCo = value; }
+        }
+
+        /// Name of the selected source game object property.
+        [SerializeField]
+        private string _sourcePropName;
+
+        public string SourcePropName {
+            get { return _sourcePropName; }
+            set { _sourcePropName = value; }
+        }
+
+        /// Name of the selected target game object selected property.
+        // TODO This shouldn't be serialized.
+        [SerializeField]
+        private string _targetPropName;
+
+        public string TargetPropName {
+            get { return _targetPropName; }
+            set { _targetPropName = value; }
+        }
+
+        /// Index of the source property in the property array.
+        ///
+        /// Property array contains names of all properties
+        /// found in the source game object.
+        [SerializeField]
+        private int _sourcePropIndex = 0;
+
+        public int SourcePropIndex {
+            get { return _sourcePropIndex; }
+            set { _sourcePropIndex = value; }
+        }
+
+        /// Index of the target property in the property array.
+        ///
+        /// Property array contains names of all properties
+        /// found in the target game object.
+        [SerializeField]
+        private int _targetPropIndex = 0;
+
+        public int TargetPropIndex {
+            get { return _targetPropIndex; }
+            set { _targetPropIndex = value; }
+        }
+
+        /// Metadata of the selected source property.
+        private PropertyInfo _sourcePropInfo;
+
+        /// Trigger that causes some change in the target component.
+        [SerializeField]
+        private Triggers _trigger;
+
+        /// Action to be performed on the target.
+        [SerializeField]
+        private Actions _action;
+
+        /// Value of the property that acts as a trigger.
+        [SerializeField]
+        private float _conditionValue;
+
+        private void Awake() {
+            // Initialize class fields.
+            _sourcePropInfo = _sourceCo.GetType().GetProperty(_sourcePropName);
+        }
+
+        // TODO Add option to update in FixedUpdate().
+        private void Update() {
+            if (_sourcePropInfo == null) return;
+
+            // Value of the source property.
+            var sourceType = _sourcePropInfo.PropertyType;
+            // Type of the source property value.
+            var sourceValue = _sourcePropInfo.GetValue(_sourceCo, null);
+
+            // Handle trigger option.
+            switch (_trigger) {
+                case Triggers.Equal:
+                    HandleEqual(sourceValue, sourceType);
+                    break;
+                case Triggers.EqualOrLess:
+                    HandleEqualOrLess(sourceValue, sourceType);
+                    break;
+                case Triggers.LessThan:
+                    HandleLessThan(sourceValue, sourceType);
+                    break;
+                case Triggers.MoreThan:
+                    HandleMoreThan(sourceValue, sourceType);
+                    break;
+            }
+        }
+
+        /// Handle 'Equal' option.
+        private void HandleEqual(object sourceValue, Type sourceType) {
+            // Check source type and then compare with the _conditionValue.
+            switch (sourceType.ToString()) {
+                case "System.Int32":
+                    // Keep source value as integer.
+                    var intValue = (int) sourceValue;
+                    if (intValue == (int) _conditionValue) {
+                        HandleEnableAction();
+                    }
+                    break;
+                case "System.Single":
+                    var floatValue = (float) sourceValue;
+                    if (floatValue == (float) _conditionValue) {
+                        HandleEnableAction();
+                    }
+                    break;
+            }
+        }
+
+        /// Handle 'EqualOrLess' option.
+        private void HandleEqualOrLess(object sourceValue, Type sourceType) {
+            // Check source type and then compare with the _conditionValue.
+            switch (sourceType.ToString()) {
+                case "System.Int32":
+                    // Keep source value as integer.
+                    int intValue;
+                    intValue = (int) sourceValue;
+                    if (intValue <= (int) _conditionValue) {
+                        HandleEnableAction();
+                    }
+                    break;
+                case "System.Single":
+                    float floatValue;
+                    floatValue = (float) sourceValue;
+                    if (floatValue <= (float) _conditionValue) {
+                        HandleEnableAction();
+                    }
+                    break;
+            }
+        }
+
+        /// Handle 'LessThan' option.
+        private void HandleLessThan(object sourceValue, Type sourceType) {
+            // Check source type and then compare with the _conditionValue.
+            switch (sourceType.ToString()) {
+                case "System.Int32":
+                    // Keep source value as integer.
+                    int intValue;
+                    intValue = (int) sourceValue;
+                    if (intValue < (int) _conditionValue) {
+                        HandleEnableAction();
+                    }
+                    break;
+                case "System.Single":
+                    float floatValue;
+                    floatValue = (float) sourceValue;
+                    if (floatValue < (float) _conditionValue) {
+                        HandleEnableAction();
+                    }
+                    break;
+            }
+        }
+
+        /// Handle 'MoreThan' option.
+        private void HandleMoreThan(object sourceValue, Type sourceType) {
+            // Check source type and then compare with the _conditionValue.
+            switch (sourceType.ToString()) {
+                case "System.Int32":
+                    // Keep source value as integer.
+                    int intValue;
+                    intValue = (int) sourceValue;
+                    if (intValue > (int) _conditionValue) {
+                        HandleEnableAction();
+                    }
+                    break;
+                case "System.Single":
+                    float floatValue;
+                    floatValue = (float) sourceValue;
+                    if (floatValue > (float) _conditionValue) {
+                        HandleEnableAction();
+                    }
+                    break;
+            }
+        }
+
+        private void HandleEnableAction() {
+            _targetCo.gameObject.SetActive(true);
+        }
+
+    }
+
+}
---
+++ WatchPropertyEditor.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Reflection;
+using OneDayGame;
+
+namespace WatchProperty {
+
+    [CustomEditor(typeof (WatchProperty))]
+    public class WatchPropertyEditor : Editor {
+
+        /* Serialized properties */
+        private SerializedProperty _sourceCo;
+        private SerializedProperty _targetCo;
+        private SerializedProperty _trigger;
+        private SerializedProperty _action;
+        private SerializedProperty _conditionValue;
+
+        /* Component properties */
+        private PropertyInfo[] _sourceProperties;
+        private PropertyInfo[] _targetProperties;
+
+        private void OnEnable() {
+            _sourceCo = serializedObject.FindProperty("_sourceCo");
+            _targetCo = serializedObject.FindProperty("_targetCo");
+            _trigger = serializedObject.FindProperty("_trigger");
+            _action = serializedObject.FindProperty("_action");
+            _conditionValue = serializedObject.FindProperty("_conditionValue");
+        }
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+            WatchProperty script = (WatchProperty) target;
+
+            // Display fields for game object.
+            EditorGUILayout.PropertyField(_sourceCo);
+
+            // Component properties by name.
+            string[] sourcePropNames;
+            // Find component properties in a selected component.
+            if (script.SourceCo) {
+                // Get all properties from source game object.
+                _sourceProperties = script.SourceCo.GetType().GetProperties();
+                // Initialize array.
+                sourcePropNames = new string[_sourceProperties.Length];
+                // Fill array with property names.
+                for (int i = 0; i < _sourceProperties.Length; i++) {
+                    sourcePropNames[i] = _sourceProperties[i].Name;
+                }
+                // Display dropdown component property list.
+                script.SourcePropIndex = EditorGUILayout.Popup(
+                    "Source Property",
+                    script.SourcePropIndex,
+                    sourcePropNames);
+
+                // Save selected property name.
+                script.SourcePropName = sourcePropNames[script.SourcePropIndex];
+            }
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUIUtility.labelWidth = 50;
+            // Display trigger dropdown field.
+            EditorGUILayout.PropertyField(_trigger);
+            EditorGUIUtility.labelWidth = 100;
+            // Display textbox to enter value required by the trigger option.
+            EditorGUILayout.PropertyField(_conditionValue);
+            EditorGUIUtility.labelWidth = 0;
+            EditorGUILayout.EndHorizontal();
+
+            // Display action dropdown field.
+            EditorGUILayout.PropertyField(_action);
+
+            // Action dropdown
+            switch (_action.enumValueIndex) {
+                // Action "Enable".
+                case (int) WatchProperty.Actions.Enable:
+                    // Display fields for target object.
+                    EditorGUILayout.PropertyField(_targetCo);
+                    break;
+                // Action "Disable".
+                case (int) WatchProperty.Actions.Disable:
+                    break;
+                // Action "Set".
+                case (int) WatchProperty.Actions.Set:
+                    // Display field for target game object.
+                    EditorGUILayout.PropertyField(_targetCo);
+
+                    // TODO Create method with args.
+                    // Component properties by name.
+                    string[] targetPropNames;
+                    // Find component properties.
+                    if (script.TargetCo) {
+                        _targetProperties = script.TargetCo.GetType()
+                            .GetProperties(
+                                BindingFlags.Public |
+                                BindingFlags.Instance);
+                        //BindingFlags.NonPublic);
+                        // Initialize array.
+                        targetPropNames = new string[_targetProperties.Length];
+                        for (int i = 0; i < _targetProperties.Length; i++) {
+                            targetPropNames[i] = _targetProperties[i].Name;
+                        }
+                        // Display dropdown component property list.
+                        script.TargetPropIndex = EditorGUILayout.Popup(
+                            "Target Property",
+                            script.TargetPropIndex,
+                            targetPropNames);
+
+                        script.TargetPropName =
+                            targetPropNames[script.TargetPropIndex];
+                    }
+                    break;
+            }
+
+            serializedObject.ApplyModifiedProperties();
+            // Save changes
+            if (GUI.changed) {
+                EditorUtility.SetDirty(script);
+            }
+        }
+
+    }
+
+}
